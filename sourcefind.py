5dd1fa32138044d2b96ea739ee1703d5da020079
Alex Clarke <informationcake@gmail.com>
2020-06-09 11:45:03 +0100

    Update sourcefind.py

--- sourcefind.py
+++ sourcefind.py
@@ -1,18 +1,20 @@
+# Python3.6. Written by Alex Clarke. 
+# Run PyBDSF to find sources in a fits image. Also do some hyperparameter tuning.
 
 import bdsf
 from memory_profiler import profile
 
-# function allows it to be profiled with @profile
-def run_pybdsf(file):
-    img = bdsf.process_image(file, adaptive_rms_box=True, advanced_opts=True,\
+# function allows it to be profiled with @profile. Run as 'mprof run sourcefind.py'. Then 'mprof plot' to get RAM vs time plot.
+@profile
+def do_pybdsf(imagename):
+    img = bdsf.process_image(imagename, adaptive_rms_box=True, advanced_opts=True,\
         atrous_do=False, psf_vary_do=True, psf_snrcut=5.0, psf_snrcutstack=10.0,\
         output_opts=True, output_all=True, opdir_overwrite='append', beam=(4.1666e-4, 4.1666e-4, 0),\
         blank_limit=None, thresh='hard', thresh_isl=5.0, thresh_pix=7.0, psf_snrtop=0.30)
     return img
 
-
 if __name__ == '__main__':
 
-    file = "560mhz8hours.fits"
-    @profile
-    run_pybdsf(file)
+    imagename = 'a560mhz8hours.fits'
+    
+    do_pybdsf(imagename)
