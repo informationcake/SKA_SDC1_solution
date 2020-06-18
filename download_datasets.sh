fda32aeed237ad50160d788e7fc87e0285d667bb
Alex Clarke <informationcake@gmail.com>
2020-06-18 12:39:13 +0100

    Create download_datasets.sh

---
+++ download_datasets.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# 8 hour data
+wget -O 560mhz8hours.fits https://owncloud.ia2.inaf.it/index.php/s/H1rAR0A9qmXBbB5/download
+wget -O 1400mhz8hours.fits https://owncloud.ia2.inaf.it/index.php/s/FS2pmOFQCn2yL06/download
+wget -O 9200mhz8hours.fits https://owncloud.ia2.inaf.it/index.php/s/EghzfsEf1CGXVsH/download
+
+# 1000 hour data
+wget -O 560mhz1000hours.fits https://owncloud.ia2.inaf.it/index.php/s/hbasFhd4YILNkCr/download
+wget -O 1400mhz1000hours.fits https://owncloud.ia2.inaf.it/index.php/s/GsxoTyv1zrdRTu4/download
+wget -O 9200mhz1000hours.fits https://owncloud.ia2.inaf.it/index.php/s/nK8Pqf3XIaXFuKD/download
