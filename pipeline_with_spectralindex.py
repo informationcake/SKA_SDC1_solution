66021efd3ddd04ff4fe2f0cfeb15ec1d77846ba0
Alex Clarke <informationcake@gmail.com>
2020-07-01 13:36:43 +0100

    Update pipeline_with_spectralindex.py

--- pipeline_with_spectralindex.py
+++ pipeline_with_spectralindex.py
@@ -22,6 +22,7 @@ import montage_wrapper as montage
 # list of functions
 # load/save pickle objects
 # make_2d_fits
+# do_primarybeam_correction
 # crop_560MHz_to1400MHz
 # regrid_montage
 # make_image_cube
@@ -70,6 +71,45 @@ def make_2d_fits(fits_image_name):
 
 
 
+def do_primarybeam_correction(pbname, imagename):
+    print(' Applying primary beam correction to image...')
+    image = fits.open(imagename)[0]
+    pb = fits.open(pbname)[0]
+    wcs = WCS(pb.header)
+    # cutout pb field of view to match image field of view
+    x_size = image.header['NAXIS1']
+    x_pixel_deg = image.header['CDELT2'] # CDELT1 is negative, so take positive one
+    size = (x_size*x_pixel_deg*u.degree, x_size*x_pixel_deg*u.degree) # angular size of cutout, using astropy coord. approx 32768*0.6 arcseconds.
+    position = (pb.header['CRVAL1']*u.degree, pb.header['CRVAL2']*u.degree) # RA and DEC of beam PB pointing
+    print(' Cutting out image FOV from primary beam image...')
+    cutout = Cutout2D(pb.data, position=position, size=size, mode='trim', wcs=wcs, copy=True)
+    pb.data = cutout.data # Put the cutout image in the FITS HDU
+    pb.header.update(cutout.wcs.to_header()) # Update the FITS header with the cutout WCS
+    pb.writeto(pbname[:-5]+'_PBCOR.fits', overwrite=True) # Write the cutout to a new FITS file
+
+    # regrid PB image cutout to match pixel scale of the image FOV
+    print(' Regredding image...')
+    # get header of image to match PB to
+    montage.mGetHdr(imagename, 'hdu_tmp.hdr')
+    # regrid pb image (270 pixels) to size of ref image (32k pixels)
+    montage.reproject(in_images=pbname[:-5]+'_PBCOR.fits', out_images=pbname[:-5]+'_PBCOR_regrid.fits', header='hdu_tmp.hdr', exact_size=True)
+    os.remove('hdu_tmp.hdr') # get rid of header text file saved to disk
+
+    # do pb correction
+    pb = fits.open(pbname[:-5]+'_PBCOR_regrid.fits')[0]
+    # fix nans introduced in primary beam by montage at edges
+    print(pb.data)
+    mask = np.isnan(pb.data)
+    pb.data[mask] = np.interp(np.flatnonzero(mask), np.flatnonzero(~mask), pb.data[~mask])
+    image.data = image.data / pb.data
+    image.writeto(imagename[:-5]+'_PBCOR.fits', overwrite=True)
+
+    
+    
+    # ------ ------ ------ ------ ------ ------ ------ ------ ------ ------
+
+
+
 def crop_560MHz_to1400MHz(fits_image_name):
     print(' Cropping image...')
     # Adapted from https://docs.astropy.org/en/stable/nddata/utils.html
@@ -98,8 +138,8 @@ def convolve_regrid(fits_image_input, fits_image_ref):
     cdelt2_560 = hdu560.header['CDELT2']
     # convolve size
     sigma = cdelt2_560/cdelt2_1400
-    # By default, the Gaussian kernel will go to 4 sigma in each direction. x_size=8
-    psf = Gaussian2DKernel(sigma, x_size=16, y_size=16, mode='oversample', factor=10)
+    # By default, the Gaussian kernel will go to 4 sigma in each direction. Set to go double this.
+    psf = Gaussian2DKernel(sigma/2, x_size=16, y_size=16, mode='oversample', factor=10)
     hdu1400_data_convolved = convolve(hdu1400.data, psf, boundary='extend')
     hdu1400.data = hdu1400_data_convolved
     # save convolved image
@@ -169,15 +209,18 @@ if __name__ == '__main__':
     #make_2d_fits('560mhz8hours.fits')
     #make_2d_fits('1400mhz8hours.fits')
     
+    do_primarybeam_correction('560mhz_primarybeam.fits', '560mhz8hours.fits')
+    do_primarybeam_correction('1400mhz_primarybeam.fits', '1400mhz8hours.fits')
+    
     # crop 560 MHz image to size of 1400 MHz image
-    #crop_560MHz_to1400MHz('560mhz8hours_2d.fits')
+    crop_560MHz_to1400MHz('560mhz8hours_2d_PBCOR.fits')
     
     # Convolve and regrid 1400 MHz image to match that of the 560 MHz image. Uses Montage.
-    convolve_regrid('1400mhz8hours_2d.fits', '560mhz8hours_2d_CropTo1400mhzFOV.fits')
+    convolve_regrid('1400mhz8hours_2d_PBCOR.fits', '560mhz8hours_2d_PBCOR_CropTo1400mhzFOV.fits')
     
     # load images now at same resolution, same sky area, same pixel size
-    hdu560 = fits.open('560mhz8hours_2d_CropTo1400mhzFOV.fits')[0]
-    hdu1400 = fits.open('1400mhz8hours_2d_convolved_regrid.fits')[0]
+    hdu560 = fits.open('560mhz8hours_2d_PBCOR_CropTo1400mhzFOV.fits')[0]
+    hdu1400 = fits.open('1400mhz8hours_2d_PBCOR_convolved_regrid.fits')[0]
     # make image cube 'cube_560_1400.fits'
     make_image_cube(hdu560, hdu1400)
     
